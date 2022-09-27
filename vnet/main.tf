a513cdecaac461bf28eededb7a0dd85acb2f48dd
1shar <kiwioo39@gmail.com>
2022-09-27 14:22:06 +0900

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "network" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.network.name
   location            = var.location
   address_space       = length(var.address_spaces) == 0 ? [var.address_space] : var.address_spaces
   dns_servers         = var.dns_servers
