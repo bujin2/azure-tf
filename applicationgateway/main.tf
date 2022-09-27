6d9df02a7ce0b920044d23d61a68393c4119704e
1shar <kiwioo39@gmail.com>
2022-09-27 15:36:14 +0900

    Update main.tf

--- applicationgateway/main.tf
+++ applicationgateway/main.tf
@@ -22,41 +22,41 @@ resource "azurerm_application_gateway" "agw" {
     subnet_id = var.agw_subnet_name
   }
 
-#   frontend_port {
-#     name = local.frontend_port_name
-#     port = 80
-#   }
+  frontend_port {
+    name = "${var.agw_name}-fe-port"
+    port = 80
+  }
 
   frontend_ip_configuration {
-    name                 = "${var.agw_name}-pip"
+    name                 = "${var.agw_name}-feip"
     public_ip_address_id = azurerm_public_ip.agw.id
   }
 
-#   backend_address_pool {
-#     name = local.backend_address_pool_name
-#   }
+  backend_address_pool {
+    name = "${var.agw_name}-beap"
+  }
 
-#   backend_http_settings {
-#     name                  = local.http_setting_name
-#     cookie_based_affinity = "Disabled"
-#     path                  = "/path1/"
-#     port                  = 80
-#     protocol              = "Http"
-#     request_timeout       = 60
-#   }
+  backend_http_settings {
+    name                  = "${var.agw_name}-be-htst"
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
 
-#   http_listener {
-#     name                           = local.listener_name
-#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
-#     frontend_port_name             = local.frontend_port_name
-#     protocol                       = "Http"
-#   }
+  http_listener {
+    name                           = "${var.agw_name}-httplstn"
+    frontend_ip_configuration_name = "${var.agw_name}-feip"
+    frontend_port_name             = "${var.agw_name}-fe-port"
+    protocol                       = "Http"
+  }
 
-#   request_routing_rule {
-#     name                       = local.request_routing_rule_name
-#     rule_type                  = "Basic"
-#     http_listener_name         = local.listener_name
-#     backend_address_pool_name  = local.backend_address_pool_name
-#     backend_http_settings_name = local.http_setting_name
-#   }
+  request_routing_rule {
+    name                       = "${var.agw_name}-rqrt"
+    rule_type                  = "Basic"
+    http_listener_name         = "${var.agw_name}-httplstn"
+    backend_address_pool_name  = "${var.agw_name}-beap"
+    backend_http_settings_name = "${var.agw_name}-be-htst"
+  }
 }
