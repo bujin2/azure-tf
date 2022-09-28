ffd2295befd22716b916fc12b85dbc8260f8534a
1shar <kiwioo39@gmail.com>
2022-09-28 13:33:02 +0900

    Update main.tf

--- database-for-maria/main.tf
+++ database-for-maria/main.tf
@@ -1,11 +1,3 @@
-resource "azurerm_subnet" "mariadb" {
-  name                 = var.subnet_name
-  resource_group_name = var.resource_group_name
-  virtual_network_name = var.virtual_network_name 
-  address_prefixes     = var.address_prefixes
-  service_endpoints    = ["Microsoft.Sql"]
-}
-
 resource "azurerm_mariadb_server" "mariadb" {
   name                = var.mariadb_name
   location            = var.location
