381c6a45f26e1759609c32c714b2d86d0159722b
1shar <kiwioo39@gmail.com>
2022-09-28 18:07:29 +0900

    Update main.tf

--- vm-linux/main.tf
+++ vm-linux/main.tf
@@ -93,13 +93,13 @@ resource "azurerm_virtual_machine" "vm-linux" {
 #       }
     }
 
-    dynamic ssh_keys {
-      for_each = var.enable_ssh_key ? var.ssh_key_values : []
-      content {
-        path     = "/home/${var.admin_username}/.ssh/authorized_keys"
-        key_data = ssh_keys.value
-      }
-    }
+#     dynamic ssh_keys {
+#       for_each = var.enable_ssh_key ? var.ssh_key_values : []
+#       content {
+#         path     = "/home/${var.admin_username}/.ssh/authorized_keys"
+#         key_data = ssh_keys.value
+#       }
+#     }
 
   }
 
