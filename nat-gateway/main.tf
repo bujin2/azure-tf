783964f1c20e5b553abebc7f7a6486123130ad58
1shar <kiwioo39@gmail.com>
2022-10-04 09:52:06 +0900

    Update main.tf

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -1,24 +1,19 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_public_ip" "natgw" {
+  name                = "${var.natgw_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.allocation_method
+  sku                 = var.sku
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-PIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_nat_gateway" "example" {
-  name                = "example-NatGateway"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_nat_gateway" "natgw" {
+  name                = var.natgw_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   sku_name            = "Standard"
 }
 
-resource "azurerm_nat_gateway_public_ip_association" "example" {
-  nat_gateway_id       = azurerm_nat_gateway.example.id
-  public_ip_address_id = azurerm_public_ip.example.id
+resource "azurerm_nat_gateway_public_ip_association" "natgw" {
+  nat_gateway_id       = azurerm_nat_gateway.natgw.id
+  public_ip_address_id = azurerm_public_ip.natgw.id
 }
