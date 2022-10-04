52455588e5b9bfdbb5753dc53bc17a5c2a1ccbaa
1shar <kiwioo39@gmail.com>
2022-10-05 07:50:54 +0900

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "network" {
-  name = var.resource_group_name
-  location = var.location
+  name                = var.resource_group_name
+  location            = var.location
   tags                = var.tags
 }
 
@@ -14,14 +14,14 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                                          = length(var.subnet_names)
-  name                                           = var.subnet_names[count.index]
-  resource_group_name                            = azurerm_resource_group.network.name
-  address_prefixes                               = [var.subnet_prefixes[count.index]]
-  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  count                                           = length(var.subnet_names)
+  name                                            = var.subnet_names[count.index]
+  resource_group_name                             = azurerm_resource_group.network.name
+  address_prefixes                                = [var.subnet_prefixes[count.index]]
+  virtual_network_name                            = azurerm_virtual_network.vnet.name
  # enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
 #  enforce_private_link_service_network_policies  = lookup(var.subnet_enforce_private_link_service_network_policies, var.subnet_names[count.index], false)
-  service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], [])
+  service_endpoints                               = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], [])
 
   
 }
