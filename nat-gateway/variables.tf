0ca2e1ab183fcc96e0fcb7dd4dbe64a2c6bbedd1
1shar <kiwioo39@gmail.com>
2022-10-04 10:04:34 +0900

    Update variables.tf

--- nat-gateway/variables.tf
+++ nat-gateway/variables.tf
@@ -1,18 +1,3 @@
-
-
-resource "azurerm_nat_gateway" "natgw" {
-  name                = var.natgw_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku_name            = var.natgw_sku
-}
-
-resource "azurerm_nat_gateway_public_ip_association" "natgw" {
-  nat_gateway_id       = azurerm_nat_gateway.natgw.id
-  public_ip_address_id = azurerm_public_ip.natgw.id
-}
-
-
 variable "natgw_name"{
   type  = string
   default = ""
@@ -33,12 +18,12 @@ variable "allocation_method"{
   default = ""
 }
 
-variable "var.natgw_sku"{
+variable "natgw_sku"{
   type  = string
   default = ""
 }
 
-variable "var.natpip_sku"{
+variable "natpip_sku"{
   type  = string
   default = ""
 }
