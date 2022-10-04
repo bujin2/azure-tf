c6108125c18301fc98a09d94d0ebcb2d6f3a38ca
1shar <kiwioo39@gmail.com>
2022-10-04 11:02:43 +0900

    Update main.tf

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_public_ip" "ngw" {
   resource_group_name = var.resource_group_name
   allocation_method   = var.allocation_method
   sku                 = var.natpip_sku
+  tags                = var.tags
 }
 
 resource "azurerm_nat_gateway" "ngw" {
@@ -11,6 +12,7 @@ resource "azurerm_nat_gateway" "ngw" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            = var.natgw_sku
+  tags                = var.tags
 }
 
 resource "azurerm_nat_gateway_public_ip_association" "ngw" {
